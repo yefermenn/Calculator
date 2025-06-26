5261c7e5693d6542f5caab12e0aa32623fddf3e8
jefersson alvarez <117768117+yefermenn@users.noreply.github.com>
2025-06-26 12:27:47 -0500

    corrección C CE =

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var number=0;
 var input="0";
 var output=document.getElementById("output");
+var operacion=0;
 
 function presionar(entrada,valor){
     if(entrada<12){
@@ -11,30 +12,63 @@ function presionar(entrada,valor){
             input=input += `${valor}`;
             output.value=input;
         }
-    }else if(12<entrada<17){
+    }else if(12<entrada && entrada<17){
         switch(entrada){
             case 13:
                 number=number+parseFloat(input);
                 output.value=number
                 input="0";
+                operacion=1;
                 break;
             case 14:
                 number=number-parseFloat(input);
                 output.value=number
                 input="0";
+                operacion=2;
                 break;
             case 15:
                 number=number*parseFloat(input);
                 output.value=number
                 input="0";
+                operacion=3;
                 break;
             case 16:
                 number=number/parseFloat(input);
                 output.value=number
                 input="0";
+                operacion=4;
                 break; 
         }
-    }else if(entrada==17){
-        
+    }else if(entrada===12){
+        switch(operacion){
+            case 1:
+                number=number+parseFloat(input);
+                output.value=number
+                input="0";
+                operacion=1;
+                break;
+            case 2:
+                number=number-parseFloat(input);
+                output.value=number
+                input="0";
+                operacion=2;
+                break;
+            case 3:
+                number=number*parseFloat(input);
+                output.value=number
+                input="0";
+                operacion=3;
+                break;
+            case 4:
+                number=number/parseFloat(input);
+                output.value=number
+                input="0";
+                operacion=4;
+                break;
+        }
+    }else if(entrada>16){
+        number=0;
+        output.value=number
+        input="0";
     }
 }
